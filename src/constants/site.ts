4cd553c8b45a214cf8ca4a00dc14995e43ca773b
vucic <vucic94@yahoo.com>
2026-09-11 19:58:04 +0200

    LinkedIn URL added

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,2 +1,3 @@
 export const CONTACT_EMAIL = "vg@glavasengineering.com";
 export const SITE_URL = "https://glavasengineering.com";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/vasilije/";
