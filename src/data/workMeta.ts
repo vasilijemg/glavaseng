26b0da1f8aa331671bb69683929fee95d8b5da7b
vucic <vucic94@yahoo.com>
2026-09-11 15:32:58 +0200

    Projects images added

---
+++ src/data/workMeta.ts
@@ -0,0 +1,7 @@
+export const workMeta = [
+  { slug: "2d-condensation-risk-assessment", number: "01", images: ["1.1.jpg", "1.2.jpg", "1.3.jpg", "1.4.jpg"] },
+  { slug: "solid-wall-u-value-thermal-bridge-assessment", number: "02", images: ["2.1.jpg", "2.2.jpg", "2.3.jpg", "2.4.jpg"] },
+  { slug: "curtain-wall-u-value-psi-value-assessment", number: "03", images: ["3.1.jpg", "3.2.jpg", "3.3.jpg", "3.4.jpg"] },
+  { slug: "3d-thermal-bridge-analysis-brise-soleil-connection", number: "04", images: ["4.1.jpg", "4.2.jpg"] },
+  { slug: "3d-condensation-risk-assessment", number: "05", images: ["5.1.jpg", "5.2.jpg", "5.3.jpg", "5.4.jpg"] },
+];
