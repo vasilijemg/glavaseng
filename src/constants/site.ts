96b0a80fa6a112d12b05ad833c1bb797736f9fe3
vucic <vucic94@yahoo.com>
2026-09-21 13:31:30 +0200

    Indexing fixes

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,3 +1,5 @@
 export const CONTACT_EMAIL = "vg@glavasengineering.com";
 export const SITE_URL = "https://glavasengineering.com";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/vasilije/";
+export const pagePath = (path: string) => path === "/" ? path : `${path.replace(/\/+$/, "")}/`;
+export const pageUrl = (path: string) => new URL(pagePath(path), SITE_URL).toString();
--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,11 +1,11 @@
 import type { APIRoute } from "astro";
-import { SITE_URL } from "../constants/site";
+import { pageUrl } from "../constants/site";
 import { services } from "../data/services";
 import { workMeta } from "../data/workMeta";
 
 const pages = ["/", "/services", "/work", "/about", "/contact", ...services.map((service) => `/services/${service.slug}`), ...workMeta.map((project) => `/work/${project.slug}`)];
 
 export const GET: APIRoute = () => {
-  const urls = pages.map((path) => `    <url><loc>${new URL(path, SITE_URL).toString()}</loc></url>`).join("\n");
+  const urls = pages.map((path) => `    <url><loc>${pageUrl(path)}</loc></url>`).join("\n");
   return new Response(`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>`, { headers: { "Content-Type": "application/xml; charset=utf-8" } });
 };
